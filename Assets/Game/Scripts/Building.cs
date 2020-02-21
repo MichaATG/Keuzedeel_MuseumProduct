f957029e97bd46df5b9377528ad96b111c24064f
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-02-21 16:01:29 +0100

    Added Building.cs + Unity materials

---
+++ Assets/Game/Scripts/Building.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private List<Building_BuildLocation> _BuildLocation = new List<Building_BuildLocation>();
+    [SerializeField] private bool _BuidingDone;
+
+    public void SetObject(GameObject obj)
+    {
+        for (int i = 0; i < _BuildLocation.Count; i++)
+        {
+            if(_BuildLocation[i]._NeededObject == obj)
+            {
+                if (!_BuildLocation[i]._OnLocation)
+                {
+                    Destroy(obj.GetComponent<Rigidbody>());
+                    obj.transform.position = _BuildLocation[i]._BuildLocation.transform.position;
+                    obj.transform.rotation = _BuildLocation[i]._BuildLocation.transform.rotation;
+                    _BuildLocation[i]._BuildLocation.SetActive(false);
+                    _BuildLocation[i]._OnLocation = true;
+                }
+            }
+        }
+
+        CheckBuildingComplete();
+    }
+
+    private void CheckBuildingComplete()
+    {
+        int checkcomplete = 0;
+        for (int i = 0; i < _BuildLocation.Count; i++)
+        {
+            if (_BuildLocation[i]._OnLocation)
+                checkcomplete++;
+        }
+        if (checkcomplete == _BuildLocation.Count)
+            _BuidingDone = true;
+    }
+}
+
+[System.Serializable]
+public class Building_BuildLocation
+{
+    public GameObject _BuildLocation;
+    public GameObject _NeededObject;
+    public bool _OnLocation;
+}
